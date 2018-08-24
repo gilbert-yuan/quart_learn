fe0ca704c7499ac31faf5d288273a279f7f8dc41
Gilbert <gilbert@osbzr.com>
2018-08-24 18:07:14 +0800

    初次提交。

--- odoo_sale/gunicorn.py
+++
@@ -1,3 +0,0 @@
-bind = '0.0.0.0:5000'
-reload = True
-worker_class = 'quart.worker.GunicornUVLoopWorker'
--- odoo_sale/sale.py
+++
@@ -1,8 +0,0 @@
-from quart import abort, Blueprint, current_app, jsonify, request
-
-app = Quart(__name__)
-
-@blueprint.route('/sale/', methods=['POST'])
-async def check_status():
-    return jsonify({'message': 'error'})
-
